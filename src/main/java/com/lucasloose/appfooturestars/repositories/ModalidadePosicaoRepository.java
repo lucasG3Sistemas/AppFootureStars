df848df7090179fff57f4e0cb5588ea4456d1cdd
lucas@G3Sistemas <lucas@g3sistemas.com>
2019-09-29 12:59:11 -0300

    método GET da classe ModalidadePosicao e classe SecurityConfig

--- src/main/java/com/lucasloose/appfooturestars/repositories/ModalidadePosicaoRepository.java
+++ src/main/java/com/lucasloose/appfooturestars/repositories/ModalidadePosicaoRepository.java
@@ -1,11 +1,18 @@
 package com.lucasloose.appfooturestars.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.lucasloose.appfooturestars.domain.Modalidade;
 import com.lucasloose.appfooturestars.domain.ModalidadePosicao;
 
 @Repository
 public interface ModalidadePosicaoRepository extends JpaRepository<ModalidadePosicao, Integer> {
-
+	
+	@Transactional(readOnly = true)
+	List<ModalidadePosicao> findByModalidadesIn(List<Modalidade>modalidades);
+	
 }
