29c6d7f2fda91c2c408db9a954c3874f63a1359a
lucas@G3Sistemas <lucas@g3sistemas.com>
2019-11-09 22:20:57 -0300

    Configurações

---
+++ src/main/java/com/lucasloose/appfooturestars/config/ProdConfig.java
@@ -0,0 +1,40 @@
+package com.lucasloose.appfooturestars.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.lucasloose.appfooturestars.services.DBService;
+import com.lucasloose.appfooturestars.services.EmailService;
+import com.lucasloose.appfooturestars.services.SmtpEmailService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	
+	@Bean
+	public boolean instantiateDatabase() {
+		
+		if (!strategy.equals("create")) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+	
+}
