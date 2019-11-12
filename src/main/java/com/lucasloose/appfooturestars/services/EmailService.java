360f1a35a8e2b35b2751bf7d4fd30c05b90f6509
lucas@G3Sistemas <lucas@g3sistemas.com>
2019-11-11 21:44:35 -0300

    Configurações

--- src/main/java/com/lucasloose/appfooturestars/services/EmailService.java
+++ src/main/java/com/lucasloose/appfooturestars/services/EmailService.java
@@ -5,8 +5,7 @@ import org.springframework.mail.SimpleMailMessage;
 import com.lucasloose.appfooturestars.domain.ListaObservacao;
 
 public interface EmailService {
-
-	//t
+	
 	void sendOrderConfirmationEmail(ListaObservacao listaObservacao);
 	
 	void sendEmail(SimpleMailMessage msg);
